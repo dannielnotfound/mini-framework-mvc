734bde78de8854d953c3e301f81fb5a75b7812a3
dannielnotfound <danielriber548@gmail.com>
2022-06-26 14:37:54 -0300

    First step to MVC Framwork; composer installed

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'name' => 'ns/mini-framework-mvc',
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'reference' => '6220e16115a4b3c3a85673740bc726ae6342a25a',
+        'type' => 'library',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'dev' => true,
+    ),
+    'versions' => array(
+        'ns/mini-framework-mvc' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'reference' => '6220e16115a4b3c3a85673740bc726ae6342a25a',
+            'type' => 'library',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'dev_requirement' => false,
+        ),
+    ),
+);
