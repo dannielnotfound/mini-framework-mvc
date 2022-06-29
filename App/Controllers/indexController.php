29f35e94a4af2c6155188ec32f63806a7660548f
dannielnotfound <danielriber548@gmail.com>
2022-06-29 15:29:07 -0300

    Defining a Layout and reusing it.

--- App/Controllers/indexController.php
+++ App/Controllers/indexController.php
@@ -6,15 +6,15 @@ class IndexController extends Action{
 
     public function index(){
         $this->view->dados = array('Sofa', 'Cadeira', 'Cama');
-        $this->render('index');
+        $this->render('index', 'layout1');
     }
     public function sobreNos(){
         $this->view->dados = array('Sofa', 'Cadeira', 'Cama');
-        $this->render('sobreNos');
+        $this->render('sobreNos', 'layout1');
     }
     public function contato(){
         $this->view->dados = array('Sofa', 'Cadeira', 'Cama');
-        $this->render('contato');
+        $this->render('contato', 'layout1');
     }
 
    
